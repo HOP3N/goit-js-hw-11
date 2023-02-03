5fc9441a04a793afd0b26c66f6aa0138bd6b10a4
HOP3N <pankoff97@gmail.com>
2023-02-03 19:31:54 +0200

    61

--- src/js/imageGallery.js
+++ src/js/imageGallery.js
@@ -1,4 +1,4 @@
-export const fetchImageGallery = async (inputValue, pageNum) => {
+export const imageGallery = async (inputValue, pageNum) => {
   return await fetch(
     `https://pixabay.com/api/?key=33348529-cc31cea9ad94d2befe505f822&q=${inputValue}&image_type=photo&orientation-horizontal&safesearch=true&per_page=40&page=${pageNum}`
   )
